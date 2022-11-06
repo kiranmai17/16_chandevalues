fed638eed0a5f74c1b12c05845492151c99cc961
Kothapelli Kiranmai <116304133+kiranmai17@users.noreply.github.com>
2022-11-06 11:35:16 +0530

    Add files via upload

---
+++ CHANGED.py
@@ -0,0 +1,11 @@
+"""
+DAY:SUNDAY
+DATE:6th nov 2022
+AUTHOR:KIRAN
+"""
+a=20; b=5.6;c='py';d='on'; e=9; f=8.20;g='78'
+print(a+e)
+print(a+e,type(a+e)) 
+print(a+b,type(a+b))                 
+print(a+f,type(a+f))                     
+                   
