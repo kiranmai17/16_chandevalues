4893d76bade546c75e20b696ca17b029225af1cf
Kothapelli Kiranmai <116304133+kiranmai17@users.noreply.github.com>
2022-11-06 11:33:10 +0530

    Add files via upload

---
+++ CHANGE.py
@@ -0,0 +1,13 @@
+"""
+DAY:SUNDAY
+DATE:6th nov 2022
+AUTHOR:KIRAN
+"""
+a=5; b=3.4;c='py';d='on'; e=8; f=8.4;g='58'
+print(a+e) #int+int -int #13
+print(a+e,type(a+e)) #13int
+print(a+b,type(a+b))#int+float=float
+                     #8.4 float
+#print(a+c,type(a+c))                  
+print(a+f,type(a+f))                     
+                   
